abb87c74cb56f2e0b2170c134d3d9c6bc9aa1dc2
harvards-cs50 <cs50.harvards@gmail.com>
2021-11-05 13:09:45 +0530

    2nd

--- registry.sh
+++ registry.sh
@@ -1,15 +1,21 @@
-args="${@:2}"
+new-registry(){
+    read -p "Name of new Registry : " name
+    read -p "Enter port on which '$name' to be run : " port
+
+    docker run -d -p $port --restart=always --name $name registry
+}
 
 
-for arg in $args
-do
-    if [ "${arg:0:1}" == "-" ]
-    then
-        echo 'Option here!'
-    fi
-done
+upload(){
 
-if [ $1 == 'registry' ]
-then
-    echo 'Begin process'
-fi
+    read -p "Enter image name/id : " image
+    read -p "Name the Image : " name
+    read -p "Enter registry port on which image is to be pushed : " port
+
+    docker tag $image localhost:$port/$name
+}
+
+#if [ $1 == 'registry' ]
+#then
+#    echo 'Begin process'
+#fi

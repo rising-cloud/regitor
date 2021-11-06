30e3db4331990e5836b4887e00fa210b14e08d92
harvards-cs50 <cs50.harvards@gmail.com>
2021-11-06 10:00:49 +0530

    3rd

--- registry.sh
+++ registry.sh
@@ -5,15 +5,27 @@ new-registry(){
     docker run -d -p $port --restart=always --name $name registry
 }
 
-
 upload(){
 
     read -p "Enter image name/id : " image
     read -p "Name the Image : " name
-    read -p "Enter registry port on which image is to be pushed : " port
+    read -p "Enter registry port : " port
 
     docker tag $image localhost:$port/$name
 }
+
+pull(){
+    read -p "Enter registry port : " port
+    read -p "Enter Image name : " image
+
+    docker pull localhost:$port/$image
+}
+
+stop-registry(){
+    read -p "Enter registry Name : " registry
+    docker container stop $registry
+}
+
 
 #if [ $1 == 'registry' ]
 #then

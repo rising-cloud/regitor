7b6c26b76760ac9259dfb3685dcf2941fd71f30a
harvards-cs50 <cs50.harvards@gmail.com>
2021-11-08 10:47:44 +0530

    4th

--- registry.sh
+++ registry.sh
@@ -1,8 +1,39 @@
+y=("y" "Y" "yes" "Yes")
+
 new-registry(){
     read -p "Name of new Registry : " name
     read -p "Enter port on which '$name' to be run : " port
+    
+    
+    while true
+    do
+    
+        read -p "Want to assign password to this registry ? (y/n) : " yn
+        yn=${yn,,}
 
-    docker run -d -p $port --restart=always --name $name registry
+        if [[ "$yn" == "y" || "$yn" == "yes" ]];
+        then
+            docker run -d \
+            -p $port \
+            --restart=always \
+            --name $name \
+            -v "$(pwd)"/auth:/auth \
+            -e "REGISTRY_AUTH=htpasswd" \
+            -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+            -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+            -v "$(pwd)"/certs:/certs \
+            -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+            -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
+            registry
+            return
+        elif [[ "$yn" == "n" || "$yn" == "no" ]];
+        then
+            echo docker run -d -p $port --restart=always --name $name registry
+            return
+        fi
+
+    done
+
 }
 
 upload(){
@@ -27,7 +58,8 @@ stop-registry(){
 }
 
 
-#if [ $1 == 'registry' ]
-#then
-#    echo 'Begin process'
-#fi
+if [ $1 == 'registry' ]
+then
+    echo 'Begin process'
+    "$2"
+fi

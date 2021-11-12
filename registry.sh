83d3db718f7d66fa1a4e1154abf74c5143183cbd
harvards-cs50 <cs50.harvards@gmail.com>
2021-11-12 11:30:48 +0530

    5th

--- registry.sh
+++ registry.sh
@@ -1,6 +1,5 @@
-y=("y" "Y" "yes" "Yes")
-
 new-registry(){
+
     read -p "Name of new Registry : " name
     read -p "Enter port on which '$name' to be run : " port
     
@@ -9,18 +8,26 @@ new-registry(){
     do
     
         read -p "Want to assign password to this registry ? (y/n) : " yn
+        read -p "Enter username : " username
+        read -p "Enter password : " password
         yn=${yn,,}
 
         if [[ "$yn" == "y" || "$yn" == "yes" ]];
         then
+            mkdir $name
+            mkdir $name/auth
+            docker run \
+            --entrypoint htpasswd \
+            httpd:2 -Bbn $username $password > $name/auth/htpasswd
+
             docker run -d \
-            -p $port \
+            -p 5000:5000 \
             --restart=always \
             --name $name \
             -v "$(pwd)"/auth:/auth \
             -e "REGISTRY_AUTH=htpasswd" \
             -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
-            -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+            -e REGISTRY_AUTH_HTPASSWD_PATH=/$name/auth/htpasswd \
             -v "$(pwd)"/certs:/certs \
             -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
             -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
@@ -43,6 +50,7 @@ upload(){
     read -p "Enter registry port : " port
 
     docker tag $image localhost:$port/$name
+    docker push localhost:5000/$name
 }
 
 pull(){
